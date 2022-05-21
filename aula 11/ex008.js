4c146c2d78d4100f00c1aefe6076d1f58f7fcc31
Pedrodupczak <101671506+Pedrodupczak@users.noreply.github.com>
2022-05-20 21:44:11 -0300

    Update curse JavaScript

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log (`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //condição simples
+    console.log(`voce ultrapassou a velocidade permitida, MULTADO`)
+}
+console.log(`use cinto de segurança`)
